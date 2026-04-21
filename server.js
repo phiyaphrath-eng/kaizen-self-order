491b4a6ec2c43f401141182a8b5b21997e799890
PHIYAPHRATH <phiyaphrath@PHIYAPHRATHs-iMac.local>
2026-04-21 13:24:49 -0500

    add source

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.post('/api/order', async (req, res) => {
     order: {
       location_id: SQUARE_LOCATION_ID,
       reference_id: `TABLE_${table}_${Date.now()}`,
+      source: { name: "Kaizen Self Order" },
       ticket_name: `Table ${table}`,
       line_items: lineItems,
       metadata: {
